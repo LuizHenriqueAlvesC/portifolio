707c39aaf6308f35791982c18918aa2c845e80f3
luiz <luiz980alves@gmail.com>
2024-07-30 11:00:49 -0300

    ajustando a responsividade do projeto

--- src/containers/Sobre/styles.ts
+++ src/containers/Sobre/styles.ts
@@ -6,6 +6,10 @@ export const GithubSecao = styled.div`
 
   img {
     height: 157px;
-    width: auto;
+
+    @media (max-width: 768px) {
+      height: auto;
+      width: 100%;
+    }
   }
 `
---
+++ src/themes/dark.ts
@@ -0,0 +1,17 @@
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDefundoBotao: string
+  corDaBorda: string
+}
+
+const temaDark = {
+  corPrincipal: '#fff',
+  corSecundaria: '#eee',
+  corDeFundo: '#282a35',
+  corDefundoBotao: '#4476bf',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaDark
